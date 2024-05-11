edc1314d17d2fe4c837fe3ebb1daaeb3d207ca34
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-05-11 16:53:33 -0400

    feat(api): add league update endpoint

--- src/routes/league.routes.ts
+++ src/routes/league.routes.ts
@@ -3,32 +3,47 @@ import type { FastifyPluginAsyncTypebox, Static } from '@fastify/type-provider-t
 import { Type } from '@sinclair/typebox';
 import fp from 'fastify-plugin';
 
-import { createLeague } from '../services/league.service.js';
+import { createLeague, updateLeague } from '../services/league.service.js';
 
 const League = Type.Object({
   createdAt: Type.Unsafe<Date | string>({ format: 'date-time' }),
   description: Type.String(),
-  id: Type.Optional(Type.Integer()),
+  id: Type.Integer(),
   name: Type.String(),
   updatedAt: Type.Unsafe<Date | string>({ format: 'date-time' }),
 });
 
-type LeagueType = Static<typeof League>;
-type CreateLeagueType = Omit<LeagueType, 'createdAt' | 'id' | 'updatedAt'>;
+const LeagueRequest = Type.Object({
+  description: Type.String(),
+  name: Type.String(),
+});
+
+const Params = Type.Object({
+  id: Type.Integer()
+});
+
+type League = Static<typeof League>;
+type LeagueRequest = Static<typeof LeagueRequest>;
+type Params = Static<typeof Params>;
 
 const createSchema = {
-  body: Type.Object({
-    description: Type.String(),
-    name: Type.String()
-  }),
+  body: LeagueRequest,
   response: {
     201: League,
   },
   tags: [ 'League' ]
 };
 
+const updateSchema = {
+  body: LeagueRequest,
+  response: {
+    200: League,
+  },
+  tags: [ 'League' ]
+};
+
 const router: FastifyPluginAsyncTypebox = async (server) => {
-  server.post<{ Body: CreateLeagueType, Reply: LeagueType }>(
+  server.post<{ Body: LeagueRequest, Reply: League }>(
     '/league',
     { schema: createSchema },
     async (req, res) => {
@@ -37,15 +52,15 @@ const router: FastifyPluginAsyncTypebox = async (server) => {
     }
   );
 
-  // server.put<{ Body: LeagueType, Params: Params, Reply: LeagueType }>(
-  // '/league/:id',
-  // { schema: updateSchema },
-  // async (req, res) => {
-  //     const { id } = req.params;
-  //     const update = await updateLeague(id, req.body);
-  //     res.status(200).send(update);
-  // }
-  // );
+  server.put<{ Body: League, Params: Params, Reply: League }>(
+    '/league/:id',
+    { schema: updateSchema },
+    async (req, res) => {
+      const { id } = req.params;
+      const update = await updateLeague(id, req.body);
+      res.status(200).send(update);
+    }
+  );
 };
 
 export default fp(router, {
