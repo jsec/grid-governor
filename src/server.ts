491fdf09a7a5ead70389ba4a8dbcdee104114315
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-05-14 19:42:53 -0400

    perf: add graceful shutdown to server process

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,31 @@
-import dns from 'node:dns';
+import closeWithGrace, { type CloseWithGraceAsyncCallback } from 'close-with-grace';
 
 import { createApp } from './app.js';
 import { env } from './shared/env.js';
 
-dns.setDefaultResultOrder('ipv4first');
-
 const app = createApp();
+
+const shutdownCallback: CloseWithGraceAsyncCallback = async ({
+  err, manual, signal
+}) => {
+  if (err) {
+    app.log.error({
+      err, manual, signal
+    }, 'error in graceful shutdown listeners');
+  }
+
+  await app.close();
+  app.log.info({
+    err, manual, signal
+  }, 'server process shutdown');
+};
+
+const shutdownListener = closeWithGrace({ delay: 500 }, shutdownCallback);
+
+app.addHook('onClose', (_instance, done) => {
+  shutdownListener.uninstall();
+  done();
+});
 
 app.listen({ host: env.API_HOST, port: env.API_PORT }, (err, address) => {
   if (err) {
