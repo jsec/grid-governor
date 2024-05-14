68fd88c93ee8182416ea9f954268e465c868f505
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-05-13 21:40:48 -0400

    feat: add driver router

--- src/modules/driver/types.ts
+++ src/modules/driver/types.ts
@@ -2,21 +2,21 @@ import { type Static, Type } from '@sinclair/typebox';
 
 export const Driver = Type.Object({
   createdAt: Type.Unsafe<Date | string>({ format: 'date-time' }),
-  discordId: Type.Optional(Type.String()),
+  discordId: Type.String(),
   firstName: Type.String(),
   id: Type.Integer(),
   lastName: Type.String(),
-  steamId: Type.Optional(Type.String()),
+  steamId: Type.String(),
   updatedAt: Type.Unsafe<Date | string>({ format: 'date-time' }),
 });
 
 export type Driver = Static<typeof Driver>;
 
 export const DriverRequest = Type.Object({
-  discordId: Type.Optional(Type.String()),
+  discordId: Type.String(),
   firstName: Type.String(),
   lastName: Type.String(),
-  steamId: Type.Optional(Type.String())
+  steamId: Type.String()
 });
 
 export type DriverRequest = Static<typeof DriverRequest>;
