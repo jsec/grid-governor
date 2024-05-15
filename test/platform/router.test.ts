51bd59e240368ffd76ba37df90b189bd67dea4c9
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-05-14 23:01:10 -0400

    test: convert platform endpoint tests to vitest

--- test/platform/router.test.ts
+++ test/platform/router.test.ts
@@ -1,111 +1,101 @@
-import type { FastifyInstance } from 'fastify';
+import { StatusCodes } from 'http-status-codes';
+import {
+  describe, expect, onTestFinished
+} from 'vitest';
 
-import anyTest, { type TestFn } from 'ava';
-
-import { createApp } from '../../src/app.js';
-import { db } from '../../src/db/conn.js';
 import { createPlatform } from '../../src/modules/platform/service.js';
+import { test } from '../context.js';
+import { platformBuilder } from '../data/platform.builder.js';
 
-const test = anyTest as TestFn<{ server: FastifyInstance }>;
+describe('Platform API', () => {
+  describe('POST', () => {
+    test('should return a 400 when the name is not provided', async ({ app }) => {
+      const response = await app.inject({
+        method: 'POST',
+        payload: {},
+        url: '/platform'
+      });
 
-test.before((t) => {
-  t.context.server = createApp();
-});
+      expect(response.statusCode).to.equal(StatusCodes.BAD_REQUEST);
+      expect(response.statusMessage).to.equal('Bad Request');
+    });
 
-test('POST - should return a 400 when the name is not provided', async (t) => {
-  const response = await t.context.server.inject({
-    method: 'POST',
-    payload: {},
-    url: '/platform'
+    test('should create a new platform', async ({ app, db }) => {
+      const payload = platformBuilder.one();
+
+      const response = await app.inject({
+        method: 'POST',
+        payload,
+        url: '/platform'
+      });
+
+      const body = response.json();
+
+      expect(response.statusCode).toEqual(StatusCodes.CREATED);
+      expect(body.name).to.equal(payload.name);
+      expect(body.id).to.not.be.null;
+
+      onTestFinished(async () => {
+        await db
+          .deleteFrom('platforms')
+          .where('id', '=', body.id)
+          .execute();
+      });
+    });
   });
 
-  t.is(response.statusCode, 400, 'Expected 400');
-  t.is(response.statusMessage, 'Bad Request', 'Expected Bad Request');
-});
+  describe('GET', () => {
+    test.todo('GET - should return a 404 if no platform with the given id exists');
 
-test('POST - should create a new platform', async (t) => {
-  const payload = {
-    name: 'iRenting'
-  };
+    test('should return a platform by id', async ({ app, db }) => {
+      const platform = await createPlatform(platformBuilder.one());
 
-  const response = await t.context.server.inject({
-    method: 'POST',
-    payload,
-    url: '/platform'
+      const response = await app.inject({
+        method: 'GET',
+        url: `/platform/${platform.id}`
+      });
+
+      const body = response.json();
+
+      expect(response.statusCode).to.equal(StatusCodes.OK);
+      expect(body.id).to.equal(platform.id);
+      expect(body.name).to.equal(platform.name);
+
+      onTestFinished(async () => {
+        await db
+          .deleteFrom('platforms')
+          .where('id', '=', body.id)
+          .execute();
+      });
+    });
   });
 
-  const body = response.json();
+  describe('PUT', () => {
+    test.todo('PUT - should return a 404 if no platform with the given id exists');
 
-  t.is(response.statusCode, 201, 'Expected 201');
-  t.like(body, payload);
+    test('should update a platform name', async ({ app, db }) => {
+      const platform = await createPlatform(platformBuilder.one());
 
-  t.teardown(async () => {
-    await db
-      .deleteFrom('platforms')
-      .where('id', '=', body.id)
-      .execute();
+      platform.name = 'Updated name';
+
+      const response = await app.inject({
+        method: 'PUT',
+        payload: platform,
+        url: `/platform/${platform.id}`
+      });
+
+      const body = response.json();
+
+      expect(response.statusCode).to.equal(StatusCodes.OK);
+      expect(body.id).to.equal(platform.id);
+      expect(body.name).to.equal(platform.name);
+
+      onTestFinished(async () => {
+        await db
+          .deleteFrom('platforms')
+          .where('id', '=', body.id)
+          .execute();
+      });
+    });
   });
-});
-
-test.todo('GET - should return a 404 if no platform with the given id exists');
-
-test('GET - should return a platform by id', async (t) => {
-  const platform = await createPlatform({
-    name: 'Test Platform - Get'
-  });
-
-  const response = await t.context.server.inject({
-    method: 'GET',
-    url: `/platform/${platform.id}`
-  });
-
-  const body = response.json();
-
-  t.is(response.statusCode, 200, 'Expected 200');
-  t.like(body, {
-    id: platform.id,
-    name: platform.name
-  }, 'Expected response to match database entity');
-
-  t.teardown(async () => {
-    await db
-      .deleteFrom('platforms')
-      .where('id', '=', platform.id)
-      .execute();
-  });
-});
-
-test.todo('PUT - should return a 404 if no platform with the given id exists');
-
-test('PUT - should update a platform name', async (t) => {
-  const platform = await createPlatform({
-    name: 'Test Platform - Update'
-  });
-
-  platform.name = 'Updated platform name';
-
-  const response = await t.context.server.inject({
-    method: 'PUT',
-    payload: platform,
-    url: `/platform/${platform.id}`
-  });
-
-  const body = response.json();
-
-  t.is(response.statusCode, 200, 'Expected 200');
-  t.is(body.name, platform.name, 'Expected updated platform name');
-
-  t.teardown(async () => {
-    await db
-      .deleteFrom('platforms')
-      .where('id', '=', platform.id)
-      .execute();
-  });
-});
-
-// TODO: add tests for delete
-
-test.after.always(async (t) => {
-  t.context.server.close();
-  await db.destroy();
 });
