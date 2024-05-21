fc17a1da2d2267d53cd3225abecadbc321ed7337
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-05-21 00:06:21 -0400

    test: add test coverage bindings for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    coverage: {
+      exclude: [ 'src/db/**/*.ts' ],
+      include: [ 'src/**/*.ts' ],
+      provider: 'v8'
+    },
     server: {
       deps: {
         inline: [ '@fastify/autoload' ]
